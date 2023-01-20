3475f764a106479a1b80c749d1c275dd6b035585
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-20 23:12:26 +0000

    Add option to switch grid square to random color

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,5 +1,22 @@
 INITIAL_GRID_SIZE = 16;
 
+MODE_BLACK = 'black';
+MODE_RANDOM = 'random';
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+
+function getRandomRGB() {
+    const rgb = [];
+    for (i = 0; i < 3; i++) {
+        rgb.push(getRandomInt(0, 256));
+    }
+    return `rgb(${rgb.join()})`
+}
+
 
 function _setGridSize(gridSize) {
     const root = document.documentElement;
@@ -13,36 +30,55 @@ function _removeGridSquaresPresent(grid) {
 }
 
 
-function changeGridSquareColourOnHover(e) {
+function hoverModeBlack(e) {
     e.target.classList.add('grid-square-onHover');
 }
 
 
-function _initGrid(grid, gridSize) {
+function hoverModeRandom(e) {
+    e.target.style.backgroundColor = getRandomRGB();
+}
+
+
+function _initGrid(grid, gridSize, mode) {
+    let gridSquareEventListener;
+    switch (mode) {
+        case (MODE_BLACK):
+            gridSquareEventListener = hoverModeBlack;
+            break;
+        case (MODE_RANDOM):
+            gridSquareEventListener = hoverModeRandom;
+            break;
+        default:
+            gridSquareEventListener = hoverModeBlack;
+    }
+
     for (let i = 0; i < (gridSize ** 2); i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
 
-        gridSquare.addEventListener('mouseenter', changeGridSquareColourOnHover)
+        gridSquare.addEventListener('mouseenter', gridSquareEventListener)
 
         grid.appendChild(gridSquare);
     }
 }
 
 
-function createGrid(gridSize = INITIAL_GRID_SIZE) {
+function createGrid(mode, gridSize = INITIAL_GRID_SIZE) {
     _setGridSize(gridSize);
 
     const grid = document.querySelector('.grid');
 
     _removeGridSquaresPresent(grid);
 
-    _initGrid(grid, gridSize);
+    _initGrid(grid, gridSize, mode);
 
 }
 
-function redrawGridFactory() {
+
+function redrawGridFactory(mode) {
     let currentGridSizeChoice = INITIAL_GRID_SIZE;
+    let currentMode = mode;
 
     function redrawGrid() {
         gridSizeChoice = Number(prompt('Choose grid size (max grid size = 100):', currentGridSizeChoice));
@@ -53,19 +89,30 @@ function redrawGridFactory() {
             gridSizeChoice = 100;
         }
         currentGridSizeChoice = gridSizeChoice;
-        createGrid(gridSizeChoice);
+        createGrid(currentMode, currentGridSizeChoice);
     }
 
-    return redrawGrid;
+    function changeMode(e) {
+        currentMode = e.target.value;
+        createGrid(currentMode, currentGridSizeChoice);
+    }
+
+    return [redrawGrid, changeMode];
 }
 
 
 function main() {
-    createGrid();
+    const defaultMode = document.querySelector('.gridMode-input:checked').value;
 
-    const gridSizeChanger = document.querySelector('.gridSizeChanger');
-    redrawGridFn = redrawGridFactory();
-    gridSizeChanger.addEventListener('click', redrawGridFn);
+    createGrid(defaultMode);
+
+    [redrawGridFn, changeModeFn] = redrawGridFactory(defaultMode);
+
+    const gridRedrawer = document.querySelector('.gridRedrawer');
+    gridRedrawer.addEventListener('click', redrawGridFn);
+
+    const gridModes = document.querySelectorAll('.gridMode-input');
+    gridModes.forEach(gridMode => gridMode.addEventListener('change', changeModeFn));
 }
 
 
